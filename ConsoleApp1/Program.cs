6877a19c78d72146e7decffb5dc8ebd550ac3e70
limhaichorn <chorn.limhai@skyvva.com>
2024-10-30 00:17:06 +0700

    commit to git

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string[] inputText = {
+            "[BE][FE][Urgent] there is error",
+            "[BE][QA][HAHA][Urgent] there is error"
+        };
+
+        foreach (var input in inputText)
+        {
+            string outputText = findNotificationChannel(input);
+            Console.WriteLine(outputText);
+        }
+    }
+
+    static string findNotificationChannel(string text)
+    {
+        // declare list notification channels
+        List<string> channels = new List<string> { "BE", "FE", "QA", "Urgent" };
+
+        // Use regex to find all tags in square brackets
+        MatchCollection matches = Regex.Matches(text, @"\[(.*?)\]");
+        List<string> returnChannels = new List<string>();
+
+        foreach (Match match in matches)
+        {
+            string tag = match.Groups[1].Value;
+            if (channels.Contains(tag))
+            {
+                returnChannels.Add(tag);
+            }
+        }
+
+        string result = "Receive channels: " + string.Join(", ", returnChannels);
+        return result;
+    }
+}
